c7e8cceed705a5f06d3e5c3ee398c7af20d434de
DevDolphin7 <devdolphin7@outlook.com>
2025-07-04 09:37:14 +0100

    Add file

---
+++ src/components/Content.tsx
@@ -0,0 +1,12 @@
+import ListenNow from "./ListenNow";
+import "../styles/Content.scss";
+
+export default function Content() {
+  return (
+    <>
+      <section className="listen-now">
+        <ListenNow />
+      </section>
+    </>
+  );
+}
