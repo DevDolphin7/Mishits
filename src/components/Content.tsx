6cb31187de560468fd351f9b1616e6cc1d337fb0
DevDolphin7 <devdolphin7@outlook.com>
2025-07-13 11:12:21 +0100

    Add test image to news object

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,13 @@
 import ListenNow from "./ListenNow";
 import News from "./News";
+import newsImage from "../../Plan/Plan v1.png";
 import "../styles/Content.scss";
 
 export default function Content() {
   const news = {
     title: "hello, world",
+    image: newsImage,
+    imageAltText: "plan v1",
   };
 
   return (
