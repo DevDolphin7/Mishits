28f51480c0ba52ce9f616ae99a816595fa7008b1
DevDolphin7 <devdolphin7@outlook.com>
2025-07-19 18:26:01 +0100

    Add component

---
+++ src/components/Attention.tsx
@@ -0,0 +1,10 @@
+import CloudImage from "./utils/CloudImage";
+import "../styles/Attention.scss";
+
+export default function Attention() {
+  return (
+    <section id="attention">
+      <CloudImage />
+    </section>
+  );
+}
