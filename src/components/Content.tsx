7a3d45826eee5b2e8401dbcf68274d0c099403f8
DevDolphin7 <devdolphin7@outlook.com>
2025-07-19 11:04:26 +0100

    Add device state

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+import { getDevice } from "./utils/utils";
 import ListenNow from "./ListenNow";
 import News from "./News";
 import newsImage from "../../Plan/Plan v1.png";
 import "../styles/Content.scss";
 
 export default function Content() {
+  const [device, setDevice] = useState<Device>(getDevice());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDevice(getDevice());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [device]);
+
   const news = {
     title: "hello, world",
     body: ["info1", "info2", "info3"],
@@ -14,7 +27,7 @@ export default function Content() {
 
   return (
     <div id="content">
-      <ListenNow />
+      <ListenNow device={device} />
       <News news={news} />
     </div>
   );
