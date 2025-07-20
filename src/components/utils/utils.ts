9866a62dba640bff9b42ac7e5d52e206070fcec2
DevDolphin7 <devdolphin7@outlook.com>
2025-07-20 09:16:08 +0100

    Move api calls to separate file

--- src/components/utils/utils.ts
+++ src/components/utils/utils.ts
@@ -1,19 +1,7 @@
-import { Cloudinary } from "@cloudinary/url-gen";
 import sizes from "./sizes";
 
-let cloudinaryCache: Cloudinary | null = null;
-
-const getDevice = (): Device => {
+export function getDevice(): Device {
   if (window.innerWidth < sizes.mobileMaxWidth) return "mobile";
   if (window.innerWidth < sizes.tabletMaxWidth) return "tablet";
   return "monitor";
-};
-
-const getCloud = (): Cloudinary => {
-  if (!cloudinaryCache) {
-    cloudinaryCache = new Cloudinary({ cloud: { cloudName: "dmspa7m1k" } });
-  }
-  return cloudinaryCache;
-};
-
-export { getDevice, getCloud };
+}
