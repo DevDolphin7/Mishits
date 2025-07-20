620e91c186c5b3bb0f731cf1b86029886828425f
DevDolphin7 <devdolphin7@outlook.com>
2025-07-20 17:17:47 +0100

    Get news content from shared drive

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,11 +6,11 @@ import CloudImage from "./utils/CloudImage";
 import Attention from "./Attention";
 import ListenNow from "./ListenNow";
 import News from "./News";
-import newsImage from "../../Plan/Plan v1.png";
 import "../styles/Content.scss";
 
 export default function Content() {
-  const [cloudContents, setCloudContents] = useState<Sound[]>([]);
+  const [cloudSongs, setCloudSongs] = useState<Sound[]>([]);
+  const [cloudNews, setCloudNews] = useState<News[]>([]);
   const [device, setDevice] = useState<Device>(getDevice());
 
   useEffect(() => {
@@ -35,43 +35,77 @@ export default function Content() {
       console.error("Error fetching songs: ", error);
     });
 
-    Promise.all([albumsPromise, songsPromise])
-      .then(([albums, songs]: [Album[] | void, CloudResponse[] | void]) => {
-        if (!songs) {
-          console.error("No songs data available");
-          return;
-        }
-        if (!albums) {
-          console.error("No albums data available");
-          return;
-        }
-
-        const cloud = getCloud();
-
-        const formattedSongs = songs.map((song) => {
-          const album =
-            albums.filter(
-              (art) => art.album === song.context?.custom?.album
-            )[0] || null;
+    const newsPromise = getMedias("News", "image")
+      .then((images) => {
+        return images.map((image, index) => {
+          const imageComponent = (
+            <CloudImage cloudImageID={image.public_id} key={index} />
+          );
 
           return {
-            title: song.context?.custom?.caption || "Untitled",
-            artist: "Mishits",
-            album: song.context?.custom?.album || "Unknown Album",
-            albumArtwork: album.albumArtwork || null,
-            albumArtworkAlt: album.albumArtworkAlt || "Album Artwork",
-            data: cloud.video(song.public_id).format("auto"),
+            title: image.context?.custom?.caption || "Untitled News",
+            body: image.context?.custom?.body
+              ? image.context?.custom?.body.split("  ")
+              : [],
+            footer: image.context?.custom?.footer || "",
+            media: imageComponent,
+            mediaAltText: image.context?.custom?.alt || "News Image",
           };
         });
-
-        return formattedSongs;
       })
+      .catch((error) => {
+        console.error("Error fetching news images: ", error);
+      });
+
+    Promise.all([albumsPromise, songsPromise, newsPromise])
+      .then(
+        ([albums, songs, news]: [
+          Album[] | void,
+          CloudResponse[] | void,
+          News[] | void
+        ]) => {
+          if (!songs) {
+            console.error("No songs data available");
+            return;
+          }
+          if (!albums) {
+            console.error("No albums data available");
+            return;
+          }
+          if (!news) {
+            console.error("No news data available");
+            return;
+          }
+
+          const cloud = getCloud();
+
+          const formattedSongs = songs.map((song) => {
+            const album =
+              albums.filter(
+                (art) => art.album === song.context?.custom?.album
+              )[0] || null;
+
+            return {
+              title: song.context?.custom?.caption || "Untitled",
+              artist: "Mishits",
+              album: song.context?.custom?.album || "Unknown Album",
+              albumArtwork: album.albumArtwork || null,
+              albumArtworkAlt: album.albumArtworkAlt || "Album Artwork",
+              data: cloud.video(song.public_id).format("auto"),
+            };
+          });
+
+          setCloudNews(news);
+
+          return formattedSongs;
+        }
+      )
       .catch((error) => {
         console.error("Error fetching content: ", error);
       })
       .then((songsWithData) => {
         if (songsWithData) {
-          setCloudContents(songsWithData);
+          setCloudSongs(songsWithData);
         } else {
           console.error("No songs with data available");
         }
@@ -90,24 +124,18 @@ export default function Content() {
     return () => window.removeEventListener("resize", handleResize);
   }, [device]);
 
-  const news = {
-    title: "hello, world",
-    body: ["info1", "info2", "info3"],
-    footer: "See you there!",
-    media: newsImage,
-    mediaAltText: "plan v1",
-  };
-
   return (
     <div id="content">
       {device !== "mobile" ? <Attention /> : null}
-      {cloudContents.length === 0 ? (
+      {cloudSongs.length === 0 ? (
         <Loading />
       ) : (
-        <>
-          <ListenNow device={device} songs={cloudContents} />
-          <News news={news} />
-        </>
+        <ListenNow device={device} songs={cloudSongs} />
+      )}
+      {cloudNews.length === 0 ? (
+        <Loading />
+      ) : (
+        cloudNews.map((newsItem, index) => <News news={newsItem} key={index} />)
       )}
     </div>
   );
