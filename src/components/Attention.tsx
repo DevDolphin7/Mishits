a60b2c71a69bc9378735454a291c1b16474eca13
DevDolphin7 <devdolphin7@outlook.com>
2025-07-20 10:01:29 +0100

    Utilise Loading component

--- src/components/Attention.tsx
+++ src/components/Attention.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { getImages } from "./utils/api";
 import CloudImage from "./utils/CloudImage";
 import "../styles/Attention.scss";
+import Loading from "./utils/Loading";
 
 export default function Attention() {
   const [cloudImageIDs, setCloudImageIDs] = useState<string[]>([]);
@@ -19,9 +20,13 @@ export default function Attention() {
 
   return (
     <section id="attention">
-      {cloudImageIDs.map((imageID: string, index: number) => (
-        <CloudImage cloudImageID={imageID} key={index} />
-      ))}
+      {cloudImageIDs.length === 0 ? (
+        <Loading />
+      ) : (
+        cloudImageIDs.map((imageID: string, index: number) => (
+          <CloudImage cloudImageID={imageID} key={index} />
+        ))
+      )}
     </section>
   );
 }
