ab447e48db193e8042ccd9883f212e0f63fb94b3
DevDolphin7 <devdolphin7@outlook.com>
2025-07-20 15:51:24 +0100

    Add Album and CloudResponse types

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -5,16 +5,20 @@ interface Media {
   mediaAltText: string;
 }
 
+interface Album {
+  album: string;
+  albumArtwork: React.ReactElement | null;
+  albumArtworkAlt: string;
+}
+
 interface Attention extends Media {
   text?: string;
 }
 
-interface Sound {
+interface Sound extends Album {
   title: string;
   artist: string;
-  album: string;
-  albumArtwork: string;
-  data: string;
+  data: CloudinaryVideo;
 }
 
 interface News extends Media {
@@ -30,4 +34,27 @@ interface NavButtons {
 interface Sizes {
   mobileMaxWidth: number;
   tabletMaxWidth: number;
+}
+
+interface CloudResponse {
+  public_id: string;
+  version: number;
+  format: string;
+  width: number;
+  height: number;
+  type: string;
+  created_at: string;
+  context?: {
+    custom?: {
+      caption: string;
+      alt: string;
+      album?: string;
+      [key: string]: string;
+    };
+  };
+  [key: string]: string | Array | object | undefined;
+}
+
+interface CloudContent {
+  ListenNow: Sound[];
 }
