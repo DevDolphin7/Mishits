1d7ec66f764b36f0eecf02d61f66a94639d3fa7f
DevDolphin7 <devdolphin7@outlook.com>
2025-07-20 09:16:43 +0100

    Get attention image(s) through api call

--- src/components/Attention.tsx
+++ src/components/Attention.tsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
+import { getImages } from "./utils/api";
 import CloudImage from "./utils/CloudImage";
 import "../styles/Attention.scss";
 
 export default function Attention() {
+  const [cloudImageIDs, setCloudImageIDs] = useState<string[]>([]);
+
+  useEffect(() => {
+    getImages("attention")
+      .then((imageIDs) => {
+        console.log("Fetched images:", imageIDs);
+        setCloudImageIDs(imageIDs);
+      })
+      .catch((error) => {
+        console.error("Error fetching images: ", error);
+      });
+  }, []);
+
   return (
     <section id="attention">
-      <CloudImage />
+      {cloudImageIDs.map((imageID: string, index: number) => (
+        <CloudImage cloudImageID={imageID} key={index} />
+      ))}
     </section>
   );
 }
