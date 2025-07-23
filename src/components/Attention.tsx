94540492c570df1c75c48a8f7e27ea95e0420551
DevDolphin7 <devdolphin7@outlook.com>
2025-07-23 16:34:47 +0100

    Display videos (as well as images) for attention component

--- src/components/Attention.tsx
+++ src/components/Attention.tsx
@@ -1,30 +1,80 @@
 import { useState, useEffect } from "react";
-import { getMedias } from "./utils/api";
+import { AdvancedVideo } from "@cloudinary/react";
+import { getCloud, getMedias } from "./utils/api";
 import CloudImage from "./utils/CloudImage";
 import "../styles/Attention.scss";
 import Loading from "./utils/Loading";
 
 export default function Attention() {
-  const [cloudImageIDs, setCloudImageIDs] = useState<string[]>([]);
+  const [cloudMedias, setCloudMedias] = useState<CloudResponse[]>([]);
 
   useEffect(() => {
-    getMedias("attention", "image")
+    getMedias("attention", "video")
+      .then((videos) => {
+        if ("errorStatus" in videos) return getMedias("attention", "image");
+
+        setCloudMedias(videos);
+      })
       .then((images) => {
-        setCloudImageIDs(images.map((image) => image.public_id));
+        if (images === undefined) return;
+
+        if ("errorStatus" in images)
+          throw Error("No images or videos found with tag 'Attention'");
+
+        setCloudMedias(images);
       })
       .catch((error) => {
-        console.error("Error fetching images: ", error);
+        console.warn("Error fetching attention: ", error);
+        console.info("Using default attention grabber");
+
+        const defaultMedia: CloudResponse = {
+          asset_folder: "Mishits/Attention",
+          asset_id: "d8b47ac7464490c9079c931544419e1e0",
+          created_at: "2025-07-22T19:54:50Z",
+          format: "mp4",
+          height: 1080,
+          public_id: "6158366-hd_1920_1080_30fps_zajtef",
+          type: "upload",
+          version: 1753214090,
+          width: 1920,
+          context: {
+            custom: {
+              alt: "A skull chandelier in a room heavily decorated with skulls and bones",
+              caption: "Swinging from the chandelier",
+            },
+          },
+        };
+
+        setCloudMedias([defaultMedia]);
       });
   }, []);
 
   return (
     <section id="attention">
-      {cloudImageIDs.length === 0 ? (
+      {cloudMedias.length === 0 ? (
         <Loading />
       ) : (
-        cloudImageIDs.map((imageID: string, index: number) => (
-          <CloudImage cloudImageID={imageID} key={index} />
-        ))
+        cloudMedias.map((media: CloudResponse, index: number) => {
+          if (["webm", "mp4", "ogv"].includes(media.format)) {
+            const cloud = getCloud();
+            const video = cloud
+              .video(media.public_id)
+              .format("auto")
+              .quality("auto");
+
+            return (
+              <AdvancedVideo
+                cldVid={video}
+                key={index}
+                className="cloudinary-advanced-video"
+                muted
+                autoPlay
+                loop
+              />
+            );
+          }
+          return <CloudImage cloudImageID={media.public_id} key={index} />;
+        })
       )}
     </section>
   );
