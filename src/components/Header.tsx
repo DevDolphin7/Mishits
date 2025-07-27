55aab6c85ed1d43c0a53160abcf6b717b7ea60c2
DevDolphin7 <devdolphin7@outlook.com>
2025-07-27 08:35:24 +0100

    Conditionally render menu component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
+import { getDevice } from "./utils/utils";
+import sizes from "./utils/sizes";
 import Logo from "../components/Logo";
 import Menu from "./Menu";
 import Navbar from "./Navbar";
 import "../styles/Header.scss";
 
 export default function Header() {
+  const [mobile, setMobile] = useState(getDevice() === "mobile");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < sizes.mobileMaxWidth && !mobile) {
+        setMobile(true);
+      } else if (window.innerWidth > sizes.mobileMaxWidth && mobile) {
+        setMobile(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [mobile]);
+
   return (
     <header>
       <Logo />
-      <Navbar />
-      <Menu />
+      {mobile ? <Menu /> : <Navbar />}
     </header>
   );
 }
