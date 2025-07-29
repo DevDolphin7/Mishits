fb427c90f20cf2f9459ce1b11fbe0bbceb959248
DevDolphin7 <devdolphin7@outlook.com>
2025-07-29 17:11:38 +0100

    Provide DeviceContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
+import { ProvideDevice } from "./contexts/Device";
 import "./styles/App.scss";
 
 export default function App() {
   return (
-    <>
+    <ProvideDevice>
       <Header />
       <Content />
       <Footer />
-    </>
+    </ProvideDevice>
   );
 }
