1f8dce798c8b5656f66d1b6d7797cc26b57bc23a
DevDolphin7 <devdolphin7@outlook.com>
2025-07-30 15:59:42 +0100

    Change h2 tags to h1

--- src/components/utils/NavButtons.tsx
+++ src/components/utils/NavButtons.tsx
@@ -14,13 +14,13 @@ export default function NavButtons({ fiend }: { fiend: boolean }) {
   return (
     <>
       {Object.keys(navButtons).map((button, index) => (
-        <h2 key={index} onClick={() => handleNavClick(button)}>
+        <h1 key={index} onClick={() => handleNavClick(button)}>
           {!fiend
             ? navButtons[button]
             : button === "about"
             ? `${navButtons[button]} Fiends`
             : `Fiend ${navButtons[button]}`}
-        </h2>
+        </h1>
       ))}
     </>
   );
